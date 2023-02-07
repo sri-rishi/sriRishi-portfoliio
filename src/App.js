eaef83173eb352c4f62c962479c3d001cb774299
sri-rishi <sririshi2612@gmail.com>
2023-02-07 20:13:06 +0530

    add HeroSection component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { HeroSection } from './components/index';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <HeroSection />
     </div>
   );
 }
