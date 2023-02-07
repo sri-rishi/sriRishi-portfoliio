82fbfcf608f6ce889bc6f12502e88928ceb0dcf4
sri-rishi <sririshi2612@gmail.com>
2023-02-08 01:41:03 +0530

    create scrollDownMouse component

---
+++ src/components/scrollDown/ScrollDownMouse.jsx
@@ -0,0 +1,11 @@
+const ScrollDownMouse = () => {
+    return(
+        <div id="scroll-down-animation">
+            <span class="mouse">
+                <span class="move"></span>
+            </span>
+        </div>
+    )
+}
+
+export default ScrollDownMouse;
