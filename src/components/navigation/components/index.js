e685aea7961882a7024f17f9921ec29d1959cefa
sri-rishi <sririshi2612@gmail.com>
2023-02-14 00:44:54 +0530

    refactor: import and export components

---
+++ src/components/navigation/components/index.js
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+import MenuLinks from "./MenuLinks";
+import MenuToggle from "./MenuToggle";
+import MenuTabs from "./MenuTabs";
+
+export {
+    Logo,
+    MenuLinks,
+    MenuToggle, 
+    MenuTabs
+}
