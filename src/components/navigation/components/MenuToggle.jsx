86980bccdd5ea266685c33e8d2fd279092ae35be
sri-rishi <sririshi2612@gmail.com>
2023-02-14 00:41:53 +0530

    feat: create menu toggler button

---
+++ src/components/navigation/components/MenuToggle.jsx
@@ -0,0 +1,13 @@
+import { Box, Icon} from "@chakra-ui/react";
+import {BiMenuAltRight} from "react-icons/bi"
+import {FaTimes} from "react-icons/fa"
+
+const MenuToggle = ({isOpen, toggle}) => {
+    return(
+        <Box fontSize="1.8rem" display={{base: "block", md: "none"}} onClick={toggle}>
+            <Icon as={isOpen ? FaTimes : BiMenuAltRight }/>
+        </Box>
+    )
+}
+
+export default MenuToggle;
