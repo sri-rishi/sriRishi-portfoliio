1f4226141e1575605613f5a3a8b0400631bf8806
sri-rishi <sririshi2612@gmail.com>
2023-02-14 00:43:42 +0530

    feat: create navbar main component

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Box, Flex, theme } from "@chakra-ui/react";
+import { useState } from "react";
+import { Logo, MenuLinks, MenuToggle } from "./components";
+import {ThemeSwitcher} from "../index";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle =() => setIsOpen(!isOpen);
+
+    return (
+        <Flex
+            as="nav"
+            alignItems="center"
+            justify="space-between"
+            wrap="wrap"
+            p="1rem 2rem"
+            w="100%"
+            height="fit-content"
+            position="fixed"
+            zIndex="50"
+            theme={theme}
+        >
+            <Logo  />
+            <Box display="flex" alignItems="center" gap="2rem">
+                <ThemeSwitcher display={["block", "block", "none", "none"]}/>
+                <MenuToggle toggle={toggle} isOpen={isOpen} />
+            </Box>
+            <MenuLinks isOpen={isOpen}/>
+        </Flex>
+    )
+}
+
+export default Navbar;
