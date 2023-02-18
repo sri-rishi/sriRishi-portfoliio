a0e4ffb4130f8c5493858ce15362d471bae40bf0
sri-rishi <sririshi2612@gmail.com>
2023-02-18 23:56:39 +0530

    style: add transition on hover

--- src/components/projectsSection/ProjectCard.jsx
+++ src/components/projectsSection/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import { Stack, Text, Heading, Card, CardBody, Image, Box, transition} from "@chakra-ui/react"
+import { motion } from "framer-motion";
 
 const ProjectCard = ({project}) => {
     const {
@@ -9,10 +10,16 @@ const ProjectCard = ({project}) => {
     } = project
     return(
         <Card 
+            as={motion.div}
             maxW='xs'
             boxShadow="2xl"
             background="whiteAlpha.800"
             color="gray.900"
+            drag='x'
+            dragConstraints={{ left: -100, right: 100 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition='0.5s linear'
         >
             <CardBody>
                 <Image
