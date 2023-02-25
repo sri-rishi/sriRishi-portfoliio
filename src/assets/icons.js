79228b53291956b56c48844fde9dfae4f181dcac
sri-rishi <sririshi2612@gmail.com>
2023-02-25 19:42:44 +0530

    chore: import icons

---
+++ src/assets/icons.js
@@ -0,0 +1,10 @@
+import {AiOutlineTwitter, AiOutlineGithub} from "react-icons/ai";
+import {FaLinkedinIn} from "react-icons/fa";
+import {MdCopyright} from "react-icons/md";
+
+export {
+    AiOutlineTwitter,
+    AiOutlineGithub,
+    FaLinkedinIn,
+    MdCopyright
+}
