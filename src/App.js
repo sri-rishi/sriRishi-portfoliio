a9d2f5109ca69297940184a08ed2c524ad091804
sri-rishi <sririshi2612@gmail.com>
2023-02-26 18:57:00 +0530

    feat: add router to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { AboutMeSection, CoverSection, FindMe, Footer, Navbar, ProjectSection} from './components';
 import {
   ChakraProvider,
   Box,
   Grid,
 } from '@chakra-ui/react';
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <ChakraProvider>
       <Box textAlign="center" fontSize="xl" bg="gray.900" color="whiteAlpha.700">
         <Grid minH="100vh">
-          <Navbar />
-          <CoverSection />
-          <AboutMeSection />
-          <ProjectSection />
-          <FindMe />
-          <Footer />
+          <AllRoutes />
         </Grid>
       </Box>
     </ChakraProvider>
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as  Router} from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <Router>
+      <ColorModeScript />
+      <App />
+    </Router>
   </StrictMode>
 );
 
