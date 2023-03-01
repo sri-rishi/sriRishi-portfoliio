9bd5008abc3aee151eb026f32bd103e0d827a38a
sri-rishi <sririshi2612@gmail.com>
2023-03-01 19:18:48 +0530

    refactor: extract intro section in component

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,21 +1,10 @@
-import { Box, Text, Container } from "@chakra-ui/react"
+import{ MyIntro }from "./components/index";
+
 
 const About = () => {
     return(
         <>
-            <Box>
-                <Container>
-                    <Text>
-                        Know who I'm
-                    </Text>
-                    <Text>
-                        Hi There
-                    </Text>
-                    <Text>
-                        I'm Rishi Srivastava from India. I am  a web developer. Being comfortable with code allows me to rapidly prototype and validate experiences.
-                    </Text>
-                </Container>
-            </Box>
+            <MyIntro />
         </>
     )
 }
