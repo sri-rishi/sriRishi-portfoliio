e8004e73e4718f50d8357164f66aa2daa67c2f5a
sri-rishi <sririshi2612@gmail.com>
2023-03-06 20:47:20 +0530

    style: add icon import

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,11 +1,12 @@
-import {AiOutlineTwitter, AiOutlineGithub,  AiOutlineDownload } from "react-icons/ai";
+import {AiOutlineTwitter, AiOutlineGithub,  AiOutlineDownload,} from "react-icons/ai";
 import {FaLinkedinIn} from "react-icons/fa";
 import {MdCopyright} from "react-icons/md";
+
 
 export {
     AiOutlineTwitter,
     AiOutlineGithub,
+    AiOutlineDownload,
     FaLinkedinIn,
     MdCopyright,
-    AiOutlineDownload 
 }
