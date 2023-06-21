e62e64904d93de3211e05d597ab10fe896593984
sri-rishi <sririshi2612@gmail.com>
2023-06-21 17:14:14 +0530

    refactor: refactored code

--- src/components/navigation/components/MenuLinks.jsx
+++ src/components/navigation/components/MenuLinks.jsx
@@ -1,34 +1,33 @@
-import { Box, Stack} from "@chakra-ui/react";
-import MenuTabs from "./MenuTabs";
+import { Box, Stack } from '@chakra-ui/react';
+import MenuTabs from './MenuTabs';
 
-const MenuLinks = ({isOpen}) => {
-    return (
-        <Box
-            display={{ base: isOpen ? "block" : "none", md: "block" }}
-            flexBasis={{ base: "100%", md: "auto" }}
-            bg={["transparent", "#404258", "transparent", "transparent"]}
-            padding={["none", "10px 14px", "none", "none"]}
-            boxShadow={["none", "md", "none", "none"]}
-            borderRadius={["none", "1rem", "none", "none"]}
-            color={["inherit", "white", "inherit", "inherit"]}
-            height={["93vh", "fit-content"]}
-        >   
-            <Stack
-                spacing={8}
-                align="center"
-                justify={["center", "space-between", "flex-end", "flex-end"]}
-                direction={["column", "row", "row", "row"]}
-                height="inherit"
-            >
-                
-                <MenuTabs to="/">Home</MenuTabs>
-                <MenuTabs to="/about">About</MenuTabs>
-                <MenuTabs to="/project">Project</MenuTabs>
-                <MenuTabs to="/blogs">Blogs</MenuTabs>
-                <MenuTabs to="/contact">Contact</MenuTabs>
-            </Stack>
-        </Box>
-    )
-}
+const MenuLinks = ({ isOpen }) => {
+  return (
+    <Box
+      display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
+      flexBasis={{ base: '100%', md: 'auto' }}
+      bg={['transparent', '#404258', 'transparent', 'transparent']}
+      padding={['none', '10px 14px', 'none', 'none']}
+      boxShadow={['none', 'md', 'none', 'none']}
+      borderRadius={['none', '1rem', 'none', 'none']}
+      color={['inherit', 'white', 'inherit', 'inherit']}
+      height={['93vh', 'fit-content']}
+    >
+      <Stack
+        spacing={8}
+        align="center"
+        justify={['center', 'space-between', 'flex-end', 'flex-end']}
+        direction={['column', 'row', 'row', 'row']}
+        height="inherit"
+      >
+        <MenuTabs to="/">Home</MenuTabs>
+        <MenuTabs to="/about">About</MenuTabs>
+        <MenuTabs to="/projects">Projects</MenuTabs>
+        <MenuTabs to="/blogs">Blogs</MenuTabs>
+        <MenuTabs to="/contact">Contact</MenuTabs>
+      </Stack>
+    </Box>
+  );
+};
 
 export default MenuLinks;
