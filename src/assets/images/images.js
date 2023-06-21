06ad0556a2167704dc6fc698426c61140a45c30f
sri-rishi <sririshi2612@gmail.com>
2023-06-21 17:09:25 +0530

    style: changed images

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,6 +1,6 @@
 import profileImage from "./profile-image.webp";
 import socialMediaPreview from "./social-media-preview.png";
-import quizAppPreview from "./quiz-app-preview.png";
+import quizAppPreview from "./quiz-preview.jpg";
 import ecommerceAppPreview from  "./ecommerce-preview.png"
 import personWorking from "./person-working.png";
 
